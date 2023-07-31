b71031f57b11dbdbcdc1aa1d8c5d8b358647ea16
dbracic21 <dbracic21@student.foi.com>
2023-07-31 20:36:13 +0200

    fixed code

--- api/account.go
+++ api/account.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,8 +9,8 @@ import (
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner binding:required"`
-	Currency string `json:"currency binding:required, ONEOF:USD EUR"`
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
@@ -33,6 +34,34 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 		return
 	}
+	ctx.JSON(http.StatusOK, account)
+
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:i"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccounts(ctx, req.ID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+
+	}
+
 	ctx.JSON(http.StatusOK, account)
 
 }
--- api/server.go
+++ api/server.go
@@ -17,6 +17,7 @@ func NewServer(store *db.Store) *Server {
 	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
 
 	server.router = router
 	return server
